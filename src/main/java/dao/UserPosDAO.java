2e7e785ef4f4dd43548c9f067cb2773652fcbb96
Breno Rev <silvabreno462@gmail.com>
2021-07-11 14:54:45 -0300

    Criando insert no banco de dados

--- src/main/java/dao/UserPosDAO.java
+++ src/main/java/dao/UserPosDAO.java
@@ -1,5 +1,41 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import conexaojdbc.SingleConnection;
+import model.Userposjava;
+
 public class UserPosDAO {
 
+	// CRIAÇÃO DA PERSISTÊNCIA DE DADOS NO BANCO DE DADOS
+	
+	private Connection connection;
+	
+	public UserPosDAO() {
+		connection = SingleConnection.getConnection();
+	}
+	
+	public void salvar(Userposjava userposjava) {
+		// ADICIONA NO BANCO DE DADOS O COMANDO " SQL " QUE FOI DIGITADO
+		try {
+		String sql = "insert into userposjava (id,nome,email) values (?,?,?)";
+		PreparedStatement insert = connection.prepareStatement(sql);
+		
+		// DEFININDO O QUE VAI SER CADA INTERROGAÇÃO DO COMANDO
+		// PRIMEIRO INDICA QUAL A INTERROGAÇÃO E DEPOIS O VALOR
+		insert.setLong(1, userposjava.getId());
+		insert.setString(2, userposjava.getNome());
+		insert.setString(3, userposjava.getEmail());
+		
+		// PEDE PARA EXECUTAR O COMANDO
+		insert.execute();
+		
+		// SALVA NO BANCO
+		
+		connection.commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/test/java/pos_java_jdbc/pos_java_jdbc/TestaBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TestaBancoJdbc.java
@@ -3,12 +3,24 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TestaBancoJdbc {
 
+	// TESTAR A CONEXÃO COM O BANCO DE DADOS COM JUNIT
+	
 	@Test
 	public void initBanco() {
 		SingleConnection.getConnection();
+		UserPosDAO userPosDAO= new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(5L);
+		userposjava.setNome("Eliane");
+		userposjava.setEmail("elianemaria8858@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
 	}
 	
 }
